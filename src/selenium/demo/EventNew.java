ff59a4d850d4394054a5c0811d162149a60f5959
NicholasLTan <nicholas.l.tan@gmail.com>
2026-03-13 18:00:46 -0400

    Refactored EventNew to use cssSelectors instead of xpath
    TODO: make event selection optional, based on boss selection window appearing

--- src/selenium/demo/EventNew.java
+++ src/selenium/demo/EventNew.java
@@ -16,17 +16,17 @@ public class EventNew {
 		AutoBattle autoBattle = new AutoBattle();
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(600));
 		IsElementPresent ePresent = new IsElementPresent();
-		//driver.findElement(By.cssSelector("img[class='img-global-banner'][src*='treasureraid165']")).click(); // Lives Yet Unwritten
 		driver.get("https://game.granbluefantasy.jp/#event/treasureraid169"); 
-		System.out.println("Bonitomurai");
+		System.out.println("PS,the Astrals...");
 		Thread.sleep(5000);
-		driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[5]/div[1]/div[1]/div[2]/div/div[1]/div[5]/div[1]/div[2]/div/img")).click();
-		//driver.findElement(By.cssSelector("img[class='img-raid-boss']")).click();
+		//driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[5]/div[1]/div[1]/div[2]/div/div[1]/div[5]/div[1]/div[2]/div/img")).click();
+		driver.findElement(By.cssSelector("div[class='btn-event-raid group']")).click();
 		System.out.println("Raid Battle");
 		Thread.sleep(1000);
 		if (ePresent.isElementPresent(driver, By.className("pop-select-part-raid"))) {
 			System.out.println("Event boss selection");
-			driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[9]/div/div[2]/div/div[2]")).click();
+			//driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[9]/div/div[2]/div/div[2]")).click();
+			driver.findElement(By.cssSelector("div[data-part='1']")).click();
 			Thread.sleep(1000);
 		}
 		//driver.findElement(By.cssSelector("img[class='img-quest-thumb'][src*='93744']")).click();
@@ -62,27 +62,6 @@ public class EventNew {
 			attempts++;
 			System.out.println(attempts);
 		}
-
-		/*
-		 * maxAttempts = 2; // Optional: To prevent infinite loops attempts = 0;
-		 * driver.findElement(By.cssSelector("[data-group='1']")).click(); //Solo
-		 * Thread.sleep(2000); driver.findElement(By.xpath(
-		 * "/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[7]/div/div[2]/div/div/div[3]/div[4]"
-		 * )).click(); //Maniac Thread.sleep(2000);
-		 * driver.findElement(By.className("btn-usual-ok")).click(); //Maniac
-		 * Thread.sleep(2000); while (attempts < maxAttempts) {
-		 * confirmTeam.confirmTeam(wait); autoBattle.autoBattle(wait); IsElementPresent
-		 * ePresent = new IsElementPresent(); Results results = new Results();
-		 * results.results(driver, wait, true);
-		 * 
-		 * 
-		 * boolean elementExists = ePresent.isElementPresent(driver,
-		 * By.className("btn-usual-close")); if (elementExists) { Thread.sleep(1000);
-		 * driver.findElement(By.className("btn-usual-close")).click();
-		 * System.out.println("Mission Close"); Thread.sleep(2000); }
-		 * 
-		 * attempts++; System.out.println(attempts); }
-		 */
 		System.out.println("Script Complete");
 	}
 
