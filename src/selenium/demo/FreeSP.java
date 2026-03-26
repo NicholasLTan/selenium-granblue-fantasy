fee0be8592bafb782fd7386deeed6d01337eac06
NicholasLTan <nicholas.l.tan@gmail.com>
2026-03-25 23:57:08 -0400

    Streamlining FreeSP
    Replaced Thread.sleep with wait.until
    Replaced By.xpath with By.cssSelector

--- src/selenium/demo/FreeSP.java
+++ src/selenium/demo/FreeSP.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import org.openqa.selenium.*;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FreeSP {
@@ -22,14 +23,16 @@ public class FreeSP {
 		//driver.findElement(By.cssSelector("div[class^='btn-extra-quest']")).click();  //Special
 		driver.get("https://game.granbluefantasy.jp/#quest/extra");
 		System.out.println("Event SP Quests");
-		Thread.sleep(5000);
-		driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[4]/div[1]/div[2]/div/div[1]/div/div[4]")).click();  //FreeSP
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[data-key='8001']")));
+		//driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[4]/div[1]/div[2]/div/div[1]/div/div[4]")).click();  //FreeSP
+		driver.findElement(By.cssSelector("div[data-key='8001']")).click();  //FreeSP
 		System.out.println("FreeSP");
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[6]/div/div[2]/div/div[1]/div/div[4]")).click();  //Play
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[data-chapter-id='80002']")));
+		driver.findElement(By.cssSelector("div[data-chapter-id='80002']")).click();  //FreeSP
+		//driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[6]/div/div[2]/div/div[1]/div/div[4]")).click();  //Play
 		System.out.println("Play");
-		Thread.sleep(5000);
-		int maxAttempts = 200; // Optional: To prevent infinite loops
+		//Thread.sleep(5000);
+		int maxAttempts = 140; // Optional: To prevent infinite loops
 		int attempts = 0;
 		while (attempts < maxAttempts) {
 			confirmTeam.confirmTeam(wait);
