a624efdb54a47d5836850664d112e9dcc7e9536e
NicholasLTan <nicholas.l.tan@gmail.com>
2026-03-25 23:54:23 -0400

    Streamlining AngelHalo
    Replaced Thread.sleep with wait.until

--- src/selenium/demo/AngelHalo.java
+++ src/selenium/demo/AngelHalo.java
@@ -4,16 +4,20 @@ import java.time.Duration;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AngelHalo {
 
 	private void AngelVH(WebDriver driver) throws InterruptedException {
-		Thread.sleep(3000);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		//Thread.sleep(3000);
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[data-key='97'")));
 		driver.findElement(By.cssSelector("div[data-key='97'")).click();  //Angel Halo
-		Thread.sleep(1000);
+		//Thread.sleep(1000);
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[data-chapter-id='51003'")));
 		driver.findElement(By.cssSelector("div[data-chapter-id='51003'")).click(); //Very Hard
-		Thread.sleep(2000);
+		//Thread.sleep(2000);
 	}
 	
 	@Test
@@ -21,22 +25,19 @@ public class AngelHalo {
 		Login login = new Login();
 		WebDriver driver = login.login();
 		ConfirmTeam confirmTeam = new ConfirmTeam();
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(1000));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(100));
 		SemiAuto semiAuto = new SemiAuto();
 		AutoBattleNew autoBattleNew = new AutoBattleNew();
 		AutoBattle autoBattle = new AutoBattle();
 
 		driver.get("https://game.granbluefantasy.jp/#quest/extra");
-		Thread.sleep(2000);
+		//Thread.sleep(2000);
 		
-		//Angel Halo VH quest
-		
-		
-		int maxRelics = 1; // Optional: To prevent infinite loops
+		int maxRelics = 85; // Optional: To prevent infinite loops
 		int relics = 0;
 		while (relics < maxRelics) {
 			int haloCount = 0;
-			this.AngelVH(driver);
+			this.AngelVH(driver); //Angel Halo VH quest
 			while (haloCount < 10) {
 				confirmTeam.confirmTeam(wait);
 				semiAuto.semiAuto(driver, wait);
@@ -54,7 +55,7 @@ public class AngelHalo {
 
 					driver.findElement(By.className("btn-usual-next")).click();
 					System.out.println("DHalo start " + relics ); 
-					Thread.sleep(2000);
+					//Thread.sleep(2000);
 
 
 					confirmTeam.confirmTeam(wait);		  
