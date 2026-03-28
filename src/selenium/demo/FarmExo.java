e663676710e741784f0b10d3d23319f0516c1a0c
NicholasLTan <nicholas.l.tan@gmail.com>
2026-03-28 13:48:50 -0400

    Beginning fav bar farming concept
    - beginning concept for farming based on favorite bar counts
    - launch loads url via driver.get
    - replacing xpath with classname

--- src/selenium/demo/FarmExo.java
+++ src/selenium/demo/FarmExo.java
@@ -18,35 +18,48 @@ public class FarmExo {
 		AutoBattle autoBattle = new AutoBattle();
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
 		WebDriverWait longWait = new WebDriverWait(driver, Duration.ofSeconds(1000));
-		driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[2]/div[1]/div/img")).click();  //Exo Sagi banner
-		//driver.findElement(By.xpath("//div[class='btn-global-banner'][data-href$='godslayer']")).click();
+		driver.get("https://game.granbluefantasy.jp/#event/godslayer"); //Exo event URL
 
-		System.out.println("Exo Vohu");
+		System.out.println("Exo Cocytus");
 		Thread.sleep(5000);
 
-		driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[3]/div[2]/div[2]/div/div[2]/img")).click();  
+		//driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[3]/div[2]/div[2]/div/div[2]/img")).click();
+		driver.findElement(By.className("img-boss-quest")).click();
 		System.out.println("Crucible");
 		Thread.sleep(1000);
 		
-		int maxAttempts;
-		int attempts;
+		String itemID = "10584"; //Stirian Lapis for Cocytus		
+		int maxItem = 10;
+		int currItem = maxItem;
+		String itemCSS = "figure[data-item-id='" + itemID + "']";
+		IsElementPresent ePresent = new IsElementPresent();
+		boolean	elementExists = ePresent.isElementPresent(driver, By.cssSelector(itemCSS));
+		if (elementExists) {
+			WebElement item = driver.findElement(By.cssSelector(itemCSS));
+			String itemCountString = item.findElement(By.xpath("./figcaption")).getText();
+			System.out.println(itemCountString);
+			String currItemCountString = itemCountString.substring(0,itemCountString.indexOf("/"));
+			System.out.println(currItemCountString);
+			currItem = Integer.valueOf(currItemCountString);
+			System.out.println("currItem = " + currItem);
+		}
 		
-		
-		maxAttempts = 21; // Optional: To prevent infinite loops
-		attempts = 0;
+		int maxAttempts = 6; // To prevent infinite loops; 0 for infinite
+		int attempts = 0;
 		//wait.until(ExpectedConditions.urlContains("#quest/extra/event"));
-		if (attempts < maxAttempts) {
+		if (attempts < maxAttempts || maxAttempts == 0) {
 			//driver.findElement(By.cssSelector("[data-group='1']")).click(); //Solo
 			//Thread.sleep(2000);
-			driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[5]/div/div[3]/div[2]")).click(); //Play
+			//driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[5]/div/div[3]/div[2]")).click(); //Play
+			driver.findElement(By.className("btn-set-quest")).click(); //Play
 			Thread.sleep(5000);          
 			//driver.findElement(By.className("btn-usual-ok")).click(); //Maniac
 			//Thread.sleep(2000);
 		}		
-		while (attempts < maxAttempts) {
+		while (attempts < maxAttempts || maxAttempts == 0 ) {
 			confirmTeam.confirmTeam(wait);
 			autoBattle.autoBattle(driver, wait);
-			IsElementPresent ePresent = new IsElementPresent();				
+			ePresent = new IsElementPresent();				
 			Results results = new Results();
 			results.results(driver, longWait, true);
 
