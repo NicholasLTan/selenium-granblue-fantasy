f7bc7459fd01f818799f543693fc81e5ceec2fac
NicholasLTan <nicholas.l.tan@gmail.com>
2026-04-01 23:46:40 -0400

    New class for farming from Special Quests

---
+++ src/selenium/demo/FarmSpecialQuest.java
@@ -0,0 +1,45 @@
+package selenium.demo;
+import java.time.Duration;
+
+import org.openqa.selenium.*;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FarmSpecialQuest {
+	@Test
+	public void farmSpecialQuest() throws InterruptedException {
+		int maxAttempts = 20; // Optional: To prevent infinite loops
+		int attempts = 0;
+		String itemID = "5421";
+		
+		Login login = new Login();
+		WebDriver driver = login.login();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(100));
+		PlayAgain playAgain = new PlayAgain();
+		By selectOne = By.cssSelector("div[data-key='100']"); //DHalo
+		By selectTwo = By.cssSelector("div[data-chapter-id='51005']"); //DHalo play
+		By selectThree = By.cssSelector("div[class='btn-usual-ok hide-common-text']"); //DHalo popup play
+		
+		driver.get("https://game.granbluefantasy.jp/#quest/extra");
+		System.out.println("Special Quests");
+		wait.until(ExpectedConditions.elementToBeClickable(selectOne));
+		driver.findElement(selectOne).click();
+		wait.until(ExpectedConditions.elementToBeClickable(selectTwo));
+		driver.findElement(selectTwo).click();
+		wait.until(ExpectedConditions.elementToBeClickable(selectThree));
+		driver.findElement(selectThree).click();
+		System.out.println("Play");
+		playAgain.playAgain(driver, wait, itemID);
+		/*
+		 * while (attempts < maxAttempts) { confirmTeam.confirmTeam(wait);
+		 * autoBattle.autoBattle(driver, wait); IsElementPresent ePresent = new
+		 * IsElementPresent(); Results results = new Results(); if ( attempts + 1 ==
+		 * maxAttempts) { results.results(driver, wait, false); } else {
+		 * results.results(driver, wait, true); } attempts++;
+		 * System.out.println(attempts); }
+		 */
+		System.out.println("Script Complete");
+	}
+}
