081a41d6b22831044c42caa3e889ca76e72fcc21
NicholasLTan <nicholas.l.tan@gmail.com>
2026-04-05 23:51:32 -0400

    Utilize new battle class
    Moved autobattle to battle as part of new URL-based design.

--- src/selenium/demo/PlayAgain.java
+++ src/selenium/demo/PlayAgain.java
@@ -1,31 +1,40 @@
 package selenium.demo;
+import java.time.Duration;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PlayAgain {
-	public void playAgain(WebDriver driver, WebDriverWait wait, String itemID) throws InterruptedException {
+	public void playAgain(WebDriver driver, WebDriverWait longWait, String itemID) throws InterruptedException {
 		ConfirmTeam confirmTeam = new ConfirmTeam();
 		AutoBattle autoBattle = new AutoBattle();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		Battle battle = new Battle();
 		MaterialTracker materialTracker = new MaterialTracker();
 		Results results = new Results();
 		boolean nextLoop = true;
 		
 		while (nextLoop) {
 			confirmTeam.confirmTeam(wait);
-			autoBattle.autoBattle(driver, wait);
+			//autoBattle.autoBattle(driver, wait);
+			battle.battle(driver, longWait);
 			nextLoop = materialTracker.materialTracker(driver, itemID);
 			results.results(driver, wait, true);
 		}
 		results.results(driver, wait, false);
 	}
 	
-	public void playAgain(WebDriver driver, WebDriverWait wait, int maxAttempts) throws InterruptedException {
+	public void playAgain(WebDriver driver, WebDriverWait longWait, int maxAttempts) throws InterruptedException {
 		int attempts = 0;
 		ConfirmTeam confirmTeam = new ConfirmTeam();
-		AutoBattle autoBattle = new AutoBattle();		
+		AutoBattle autoBattle = new AutoBattle();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		Battle battle = new Battle();
 		while (attempts < maxAttempts) {
 			confirmTeam.confirmTeam(wait);
-			autoBattle.autoBattle(driver, wait);			
+			//autoBattle.autoBattle(driver, wait);
+			battle.battle(driver, longWait);
+
 			Results results = new Results();
 			if ( attempts + 1 == maxAttempts) {
 				results.results(driver, wait, false);
