bcc8e3cdd1a7d37f262497bf3a2361d8aaa8d0d0
NicholasLTan <nicholas.l.tan@gmail.com>
2026-04-07 15:11:09 -0400

    Added waits
    Added waits to prevent calling confirmTeam early

--- src/selenium/demo/PlayAgain.java
+++ src/selenium/demo/PlayAgain.java
@@ -2,21 +2,19 @@ package selenium.demo;
 import java.time.Duration;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PlayAgain {
 	public void playAgain(WebDriver driver, WebDriverWait longWait, String itemID) throws InterruptedException {
 		ConfirmTeam confirmTeam = new ConfirmTeam();
-		//AutoBattle autoBattle = new AutoBattle();
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 		Battle battle = new Battle();
 		MaterialTracker materialTracker = new MaterialTracker();
 		Results results = new Results();
 		boolean nextLoop = true;
-		
 		while (nextLoop) {
-			confirmTeam.confirmTeam(wait);
-			//autoBattle.autoBattle(driver, wait);
+			confirmTeam.confirmTeam(driver, wait);
 			battle.battle(driver, longWait);
 			nextLoop = materialTracker.materialTracker(driver, itemID);
 			results.results(driver, wait, true);
@@ -27,15 +25,14 @@ public class PlayAgain {
 	public void playAgain(WebDriver driver, WebDriverWait longWait, int maxAttempts) throws InterruptedException {
 		int attempts = 0;
 		ConfirmTeam confirmTeam = new ConfirmTeam();
-		//AutoBattle autoBattle = new AutoBattle();
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 		Battle battle = new Battle();
+		Results results = new Results();
 		while (attempts < maxAttempts) {
-			confirmTeam.confirmTeam(wait);
-			//autoBattle.autoBattle(driver, wait);
+			wait.until(ExpectedConditions.urlContains("supporter"));
+			confirmTeam.confirmTeam(driver, wait);
+			wait.until(ExpectedConditions.urlContains("https://game.granbluefantasy.jp/#raid"));
 			battle.battle(driver, longWait);
-
-			Results results = new Results();
 			if ( attempts + 1 == maxAttempts) {
 				results.results(driver, wait, false);
 			} else {
