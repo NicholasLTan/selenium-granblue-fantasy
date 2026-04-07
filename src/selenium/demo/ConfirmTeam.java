a95b62a42133f1848b553aa7fde1313b91520f12
NicholasLTan <nicholas.l.tan@gmail.com>
2026-04-07 15:05:36 -0400

    Created new function with more checks
    Created second version of confirmTeam function that is more robust.
    Retaining first version for now to not break compatibility until all calls are migrated.

--- src/selenium/demo/ConfirmTeam.java
+++ src/selenium/demo/ConfirmTeam.java
@@ -2,9 +2,25 @@ package selenium.demo;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class ConfirmTeam {
+	public void confirmTeam(WebDriver driver, WebDriverWait wait) {
+		WebElement confirm = null;
+		System.out.println("Start New ConfirmTeam");
+		System.out.println(driver.getCurrentUrl());
+		if (driver.getCurrentUrl().contains("supporter")) {
+			confirm = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[class='btn-usual-ok se-quest-start']")));
+		}
+		if ( confirm != null ) {
+			System.out.println("Confirm team/support");
+			confirm.click(); // Confirm team/support
+		}
+		wait.until ( ExpectedConditions.not ( ExpectedConditions.urlContains ("supporter")));
+		return;
+	}
+	
 	public void confirmTeam(WebDriverWait wait) {
 		WebElement confirm = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"wrapper\"]/div[3]/div[3]/div[3]/div[2]")));	    			    
 		System.out.println("Confirm team/support");
