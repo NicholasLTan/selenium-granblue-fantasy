684e29caea21b7b6cbd543698563becebf6f2d5c
NicholasLTan <nicholas.l.tan@gmail.com>
2026-04-06 23:58:47 -0400

    Clean up old comments

--- src/selenium/demo/Login.java
+++ src/selenium/demo/Login.java
@@ -11,17 +11,12 @@ public class Login {
 
 		boolean journeyDrops = true;
 
-		// Optional. If not specified, WebDriver searches the PATH for chromedriver.
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nick\\libs\\selenium-jars\\chromedriver-win64\\chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
-		//println File("C:\\Users\\Nick\\AppData\\Local\\Google\\Chrome\\User Data\\Default").getAbsolutePath();
 		options.setExperimentalOption("debuggerAddress","localhost:9222");
-
-
 		WebDriver driver = new ChromeDriver(options);
 		//driver.get("https://game.granbluefantasy.jp/#mypage"); 
-		Thread.sleep(1000);  // Let the user actually see something!
-		// Click on the element
+		//Thread.sleep(1000);  
 		IsElementPresent ePresent = new IsElementPresent();
 		boolean elementExists = ePresent.isElementPresent(driver, By.className("btn-usual-close"));
 		if (elementExists) {
