ce79c7e7e7a320f4f3306c5bdf883feafae3260d
NicholasLTan <nicholas.l.tan@gmail.com>
2026-04-07 15:00:55 -0400

    Implemented pre-defined raid selector
    Created a raid selector that uses pre-defined quest selection.
    Replaced xpaths with classname/css.
    Reduced wait for confirmTeam

--- src/selenium/demo/FarmUnF.java
+++ src/selenium/demo/FarmUnF.java
@@ -9,33 +9,47 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FarmUnF {
-
 	@Test
 	public void farmUnF() throws InterruptedException {
+		int maxAttempts = 30; // Optional: To prevent infinite loops;
+		int target = 3; // Selects quest. Choose from 1-5 below
 		Login login = new Login();
 		WebDriver driver = login.login();
-		ConfirmTeam confirmTeam = new ConfirmTeam();
-		AutoBattle autoBattle = new AutoBattle();
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(1000));
-		//driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[2]/div[2]/div/img")).click();  //Unite and Fight banner
-		driver.get("https://game.granbluefantasy.jp/#event/teamraid081");
-		System.out.println("UnF");
-		Thread.sleep(5000);
-		
-		//driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[4]/div[4]/img")).click();  //Easy
-		driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[4]/div[3]/img")).click();  //Nightmare
-		Thread.sleep(1000);
-		//driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[19]/div/div[2]/div/div[4]/div[3]")).click();  //Eyeball		
-		driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[20]/div/div[2]/div/div[4]/div/div/div[3]/div[1]")).click();  //NM100
-		System.out.println("Blanche");
-		Thread.sleep(5000);
+		PlayAgain playAgain = new PlayAgain();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		WebDriverWait longWait = new WebDriverWait(driver, Duration.ofSeconds(1000));
+		WebElement banner = null; //Nightmare
+		WebElement quest = null; //NM95
 
-		int maxAttempts;
-		int attempts;
+		driver.get("https://game.granbluefantasy.jp/#event/teamraid082");
+		System.out.println("UnF");
+		wait.until(ExpectedConditions.elementToBeClickable(By.className("btn-ranking-profile")));
 		
-		maxAttempts = 50; // Optional: To prevent infinite loops
-		attempts = 0;
+		if (target == 5) {banner = driver.findElement(By.className("btn-ex-raid4")); } //Nightmare;
+		else if (target == 4) {banner = driver.findElement(By.className("btn-ex-raid4")); } //Nightmare;
+		else if (target == 3) {banner = driver.findElement(By.className("btn-ex-raid2")); } //BaitFarm;
+		else if (target == 2) {banner = driver.findElement(By.className("btn-raid-select")); } //Solo;
+		else if (target == 1) {banner = driver.findElement(By.className("btn-raid-select")); } //Solo;
 		
+		
+		System.out.println(banner.findElement(By.className("img-btn-raid")).getAttribute("alt"));
+		banner.click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class*='pop-raid']")));
+		
+		if (target == 5) {quest = driver.findElement(By.cssSelector("div[data-chapter-id='94268']")); } //NM95
+		else if (target == 4) {quest = driver.findElement(By.cssSelector("div[data-chapter-id='94267']")); } //NM90
+		else if (target == 3) {quest = driver.findElement(By.cssSelector("div[data-chapter-id='94265']")); } //Extreme+
+		else if (target == 2) {quest = driver.findElement(By.cssSelector("div[data-chapter-id='94263']")); //Very Hard
+			maxAttempts = Math.min(maxAttempts, 15);}
+		else if (target == 1) {quest = driver.findElement(By.cssSelector("div[data-chapter-id='94262']")); //Hard
+			maxAttempts = Math.min(maxAttempts, 15);
+		}
+		System.out.println(quest.getAttribute("data-chapter-name"));
+		quest.click();
+		playAgain.playAgain(driver, longWait, maxAttempts);
+		
+		
+		/*
 		while (attempts < maxAttempts) {
 			confirmTeam.confirmTeam(wait);
 			autoBattle.autoBattle(driver, wait);
@@ -57,7 +71,7 @@ public class FarmUnF {
 			attempts++;
 			System.out.println(attempts);
 		}
-				
+		*/		
 		
 	}
 }
