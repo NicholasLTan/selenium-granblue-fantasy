efbc2e88792c8043c29a3e86e76f09ea4a8b6a06
NicholasLTan <nicholas.l.tan@gmail.com>
2026-04-09 13:26:45 -0400

    Replacing sleep with wait

--- src/selenium/demo/Login.java
+++ src/selenium/demo/Login.java
@@ -1,28 +1,37 @@
 package selenium.demo;
 
+import org.openqa.selenium.WebElement;
+
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Login {
 
 	public WebDriver login() throws InterruptedException {
 
-		boolean journeyDrops = true;
+		boolean journeyDrops = true; //true to enable/extend Journey Drops every run; otherwise false;
 
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nick\\libs\\selenium-jars\\chromedriver-win64\\chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.setExperimentalOption("debuggerAddress","localhost:9222");
 		WebDriver driver = new ChromeDriver(options);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 		//driver.get("https://game.granbluefantasy.jp/#mypage"); 
 		//Thread.sleep(1000);  
 		IsElementPresent ePresent = new IsElementPresent();
 		boolean elementExists = ePresent.isElementPresent(driver, By.className("btn-usual-close"));
 		if (elementExists) {
-			driver.findElement(By.className("btn-usual-close")).click();
+			WebElement element = driver.findElement(By.className("btn-usual-close"));
+			element.click();
+			
 			System.out.println("Main Quest Close");
-			Thread.sleep(2000);
+			wait.until(ExpectedConditions.stalenessOf(element));
 		}
 		if ( journeyDrops == true ) {
 			JourneyDropsActivate JDA = new JourneyDropsActivate();
