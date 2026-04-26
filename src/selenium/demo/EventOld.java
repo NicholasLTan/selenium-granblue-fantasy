3225981f99fc978ebeee94d50e0a1c497a2f193b
NicholasLTan <nicholas.l.tan@gmail.com>
2026-04-26 19:55:11 -0400

    Use new methods, make multi-maniac loop
    Made use of playAgain() instead of local loop.
    Created logic to handle multi-part events for maniac difficulty.
    Made selection more robust via data-difficulty

--- src/selenium/demo/EventOld.java
+++ src/selenium/demo/EventOld.java
@@ -1,6 +1,7 @@
 package selenium.demo;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.*;
 import org.junit.Test;
@@ -9,102 +10,79 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class EventOld {
-
 	@Test
 	public void eventOld() throws InterruptedException {
+		boolean maniac = true;
+		boolean raid = true;
+		boolean extreme = true;
+		int maxAttempts = 10; //For farming Extreme
+		
 		Login login = new Login();
 		WebDriver driver = login.login();
-		ConfirmTeam confirmTeam = new ConfirmTeam();
-		AutoBattle autoBattle = new AutoBattle();
-		SemiAuto semiAuto = new SemiAuto();
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(100));
+		PlayAgain playAgain = new PlayAgain();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		WebDriverWait longWait = new WebDriverWait(driver, Duration.ofSeconds(120));
 		//driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[2]/div[3]/div/img")).click();  //Imagination Overdrive banner
-		driver.get("https://game.granbluefantasy.jp/#quest/extra/event/11073"); 
-		System.out.println("Unbending Chivalry");  
-		System.out.println("Event SP Quests");
-		Thread.sleep(1000);
-		
-		int maxAttempts;
+		driver.get("https://game.granbluefantasy.jp/#quest/extra/event/11074"); 
+		System.out.println("ZodiaCamp 2nd");  
+		System.out.println("Event SP Quests");		
 		int attempts;
-				
-		maxAttempts = 0; // Optional: To prevent infinite loops
-		attempts = 0;		
-		if (attempts < maxAttempts) {
-			wait.until(ExpectedConditions.urlContains("#quest/extra/event"));
-			Thread.sleep(5000);
-			driver.findElement(By.cssSelector("[data-group='1']")).click(); //Solo
-			Thread.sleep(2000);
-			driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[7]/div/div[2]/div/div/div[3]/div[4]")).click(); //Maniac
-			Thread.sleep(2000);          
-			driver.findElement(By.className("btn-usual-ok")).click(); //Maniac
-			Thread.sleep(2000);
-		}		
-		while (attempts < maxAttempts) {
-			confirmTeam.confirmTeam(wait);
-			autoBattle.autoBattle(driver, wait);
-			IsElementPresent ePresent = new IsElementPresent();				
-			Results results = new Results();
-			results.results(driver, wait, true);
-			attempts++;
-			System.out.println(attempts);
-		}
-		
-		maxAttempts = 25; // Optional: To prevent infinite loops
-		attempts = 0;
-		if (attempts < maxAttempts) {
-			
-			wait.until(ExpectedConditions.urlContains("#quest/extra/event"));
-			Thread.sleep(5000);
-			driver.findElement(By.cssSelector("[data-group='1']")).click(); //Solo
-			Thread.sleep(2000);
-			driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[7]/div/div[2]/div/div/div[2]/div[4]")).click(); //Maniac+
-			Thread.sleep(2000);          
-			//driver.findElement(By.className("btn-usual-ok")).click(); //Extreme
-			//Thread.sleep(2000);
-		}		
-		while (attempts < maxAttempts) {
-			confirmTeam.confirmTeam(wait);
-			autoBattle.autoBattle(driver, wait);
-			IsElementPresent ePresent = new IsElementPresent();				
-			Results results = new Results();
-			results.results(driver, wait, true);
-			attempts++;
-			System.out.println(attempts);
-		}
-		
-		maxAttempts = 0; // Optional: To prevent infinite loops
-		attempts = 0;
-		if (attempts < maxAttempts) {
-			wait.until(ExpectedConditions.urlContains("#quest/extra/event"));
-			Thread.sleep(5000);
-			driver.findElement(By.cssSelector("[data-group='3']")).click(); //Raid
-			Thread.sleep(1000);
-			driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[7]/div/div[2]/div/div[3]/div[2]/div[4]")).click(); //Extreme+
-			Thread.sleep(2000);
-		} 
-		while (attempts < maxAttempts) {
-			confirmTeam.confirmTeam(wait);
-			//autoBattle.autoBattle(driver, wait);
-			semiAuto.semiAuto(driver, wait);
-			IsElementPresent ePresent = new IsElementPresent();				
-			Results results = new Results();
-			if ( attempts + 1 == maxAttempts) {
-				results.results(driver, wait, false);
-			} else {
-				results.results(driver, wait, true);
-			}
 
-			boolean elementExists = ePresent.isElementPresent(driver, By.className("btn-usual-close"));
-			if (elementExists) {
-				Thread.sleep(1000);
-				driver.findElement(By.className("btn-usual-close")).click();
-				System.out.println("Mission Close");
-				Thread.sleep(2000);
+		//Maniac Loop
+		if ( maniac ) {
+			for (int i=6; i<=7; i++) {
+				maxAttempts = 2; //Maniac 2x daily limit
+				attempts = 0;
+				if (attempts < maxAttempts) {
+					By cssManiac = By.cssSelector("div[data-difficulty='" + i + "']");				
+					wait.until(ExpectedConditions.urlContains("#quest/extra/event"));
+					wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[data-group='1']")));
+					driver.findElement(By.cssSelector("div[data-group='1']")).click(); //Solo
+					wait.until(ExpectedConditions.elementToBeClickable(By.className("btn-usual-close")));
+					List<WebElement> elementExists = driver.findElements(cssManiac);
+					if (elementExists.size() > 0) {
+						System.out.println("EventOld Maniac " + i + " launching.");
+						driver.findElement(cssManiac).click(); //Maniac+
+						wait.until(ExpectedConditions.elementToBeClickable(By.className("btn-usual-ok"))); //Daily limit OK
+						maxAttempts = Integer.valueOf(driver.findElement(By.className("txt-popup-body")).getText().replaceAll("[^0-9]", ""));
+						driver.findElement(By.className("btn-usual-ok")).click(); //Maniac
+					} else {
+						System.out.println("EventOld Maniac " + i + " not found.");
+						driver.findElement(By.className("btn-usual-close")).click();
+						break;
+					}
+
+				}
+				playAgain.playAgain(driver, longWait, maxAttempts);
 			}
-			attempts++;
-			System.out.println(attempts);
 		}
-		System.out.println("Script complete");		
 		
+		//Raid EX farming
+		if ( raid ) {
+			maxAttempts = 5; // Optional: To prevent infinite loops
+			attempts = 0;
+			if (attempts < maxAttempts) {
+				wait.until(ExpectedConditions.urlContains("#quest/extra/event"));
+				wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[data-group='3']")));
+				driver.findElement(By.cssSelector("div[data-group='3']")).click(); //Raid
+				wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[data-difficulty='4']")));
+				driver.findElement(By.cssSelector("div[data-difficulty='4']")).click(); //Extreme+
+			}
+			playAgain.playAgain(driver, longWait, maxAttempts);							
+		}
+		
+		//Extreme Farming
+		if ( extreme ) {
+			attempts = 0;		
+			if (attempts < maxAttempts) {
+				wait.until(ExpectedConditions.urlContains("#quest/extra/event"));
+				wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[data-group='1']")));
+				driver.findElement(By.cssSelector("div[data-group='1']")).click(); //Solo
+				wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[data-difficulty='4']")));
+				driver.findElement(By.cssSelector("div[data-difficulty='4']")).click(); //Extreme
+			}		
+			playAgain.playAgain(driver, longWait, maxAttempts);
+		}
+		System.out.println("Script complete");
 	}
 }
