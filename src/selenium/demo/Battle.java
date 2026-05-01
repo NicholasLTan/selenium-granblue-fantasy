8cc3c2cb850ef66ec45c37e8311aea1c06b7c2a3
NicholasLTan <nicholas.l.tan@gmail.com>
2026-05-01 13:16:43 -0400

    Testing purple skill activation

--- src/selenium/demo/Battle.java
+++ src/selenium/demo/Battle.java
@@ -14,6 +14,44 @@ public class Battle {
 		By popup = By.className("pop-usual");
 		AutoBattle autoBattle = new AutoBattle();
 		autoBattle.autoBattle(driver, wait);
+		//activate purple skills
+		  
+		List<WebElement> purpleSkills = driver.findElements(By.cssSelector("div[class^='lis-ability-state'][type='5'][state='2']"));
+		if ( !purpleSkills.isEmpty() ) {
+		    for ( int i = 0 ; i < purpleSkills.size() ; i++ ) {
+		        WebElement purple = purpleSkills.get(i);
+		        System.out.println("pos = " + purple.findElement(By.xpath("./../..")).getAttribute("pos"));
+		        if (!purple.findElement(By.xpath("./../..")).getAttribute("pos").isEmpty()) {
+		        	purple.click();
+		        	Thread.sleep(2500);
+		        	//wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[class='btn-ability-skip']")));
+		        	/*if ( driver.findElement(By.className("prt-ability-skip")).getAttribute("active").equals("1") ) {
+		        		driver.findElement(By.className("btn-ability-skip")).click();
+		        	}*/
+		        	
+		        	purpleSkills = driver.findElements(By.cssSelector("div[icon-type='5']"));
+		        	if ( !purpleSkills.isEmpty() ) {
+		        		purpleSkills.get(0).click();
+		        	}
+		        	driver.findElement(By.className("btn-command-back")).click();
+		        }
+		    } 
+		}
+		/*
+		if ( purpleSkills.size() > 0 ) {
+		    for ( int i = 0 ; i < purpleSkills.size() ; i++ ) {		        
+		        WebElement purple = purpleSkills.get(i);
+		        if ( !purple.getAttribute("pos").isEmpty() ) { 
+		        	purple.click();
+		        }
+		    } 
+		}
+		  */
+		  
+		  
+		  //<div class="lis-ability-state ability2" state="1" type="3"></div>
+		 
+		
 		while (driver.getCurrentUrl().startsWith("https://game.granbluefantasy.jp/#raid")) {
 			wait.until(ExpectedConditions.or(
 					ExpectedConditions.visibilityOfElementLocated(popup),
