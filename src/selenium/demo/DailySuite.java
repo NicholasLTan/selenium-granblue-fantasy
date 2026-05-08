24c7411e19831e7ab34a6b54e9fec6d4999da3aa
NicholasLTan <nicholas.l.tan@gmail.com>
2026-05-08 11:23:42 -0400

    Creation of a test suite to run test consecutively

---
+++ src/selenium/demo/DailySuite.java
@@ -0,0 +1,12 @@
+package selenium.demo;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({FarmVeritas.class, FarmRaids.class, FreeSP.class})
+//@SelectClasses({ProQuests.class, FarmVeritas.class, FarmRaids.class, FreeSP.class})
+public class DailySuite {
+
+}
+//#wrapper > div.contents > div.cnt-mypage > div.cnt-layout-v2 > div.prt-user-scene > div.prt-vyrnsampo.display-off-target.is-vyrnsampo-type3
+//<div class="btn-vyrnsampo type-3 frame-0" data-href="vyrnsampo"> </div>
