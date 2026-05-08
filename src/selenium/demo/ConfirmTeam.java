1d2d2b9fa79230ec59d30abd8947151cca75c990
NicholasLTan <nicholas.l.tan@gmail.com>
2026-05-08 11:25:40 -0400

    Handle failure to reach support page
    Also loosen classpath for ok button to handle more variants.

--- src/selenium/demo/ConfirmTeam.java
+++ src/selenium/demo/ConfirmTeam.java
@@ -2,6 +2,7 @@ package selenium.demo;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -11,13 +12,24 @@ public class ConfirmTeam {
 		System.out.println("Start New ConfirmTeam");
 		System.out.println(driver.getCurrentUrl());
 		if (driver.getCurrentUrl().contains("supporter")) {
-			confirm = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[class='btn-usual-ok se-quest-start']")));
+			confirm = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[class^='btn-usual-ok']")));
 		}
 		if ( confirm != null ) {
 			System.out.println("Confirm team/support");
 			confirm.click(); // Confirm team/support
 		}
-		wait.until ( ExpectedConditions.not ( ExpectedConditions.urlContains ("supporter")));
+		try {
+			wait.until ( ExpectedConditions.not ( ExpectedConditions.urlContains ("supporter")));
+		} catch (TimeoutException e) {
+			if ( !driver.findElements(By.className("pop-usual")).isEmpty() ) {
+				WebElement popup = driver.findElement(By.className("pop-usual"));
+				popup.findElement(By.className("btn-usual-ok")).click();
+			}
+			if (driver.getCurrentUrl().equals("https://game.granbluefantasy.jp/#mypage")) {
+				System.out.println("homepage");
+				return;
+			}
+		}
 		return;
 	}
 	
