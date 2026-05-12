17817f608af34186799bf5fce5e4c7b2d653fc8e
NicholasLTan <nicholas.l.tan@gmail.com>
2026-05-11 23:15:33 -0400

    New class to fetch provisions

---
+++ src/selenium/demo/Provisions.java
@@ -0,0 +1,24 @@
+package selenium.demo;
+
+import java.time.Duration;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Provisions {
+	@Test
+	public void provisions() throws InterruptedException{
+		Login login = new Login();
+		WebDriver driver = login.login();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		
+		driver.get("https://game.granbluefantasy.jp/#frontier");
+		wait.until(ExpectedConditions.elementToBeClickable(By.className("btn-receive-all")));
+		driver.findElement(By.className("btn-receive-all")).click();
+		wait.until(ExpectedConditions.textToBe(By.className("prt-popup-header"), "Pick Up Loot"));
+		driver.findElement(By.className("btn-usual-ok")).click();
+	}
+}
