926285f51743029b39b9680639a109fa5624b4d5
NicholasLTan <nicholas.l.tan@gmail.com>
2026-05-27 02:43:07 -0400

    Adjusting EventOld maniac logic
    Maniac quests had two quests with the same difficulty.
    Added logic to try and iterate through, while handling edge cases.

--- src/selenium/demo/EventOld.java
+++ src/selenium/demo/EventOld.java
@@ -14,18 +14,19 @@ public class EventOld {
 	public void eventOld() throws InterruptedException {
 		boolean maniac = true;
 		boolean raid = false;
-		boolean extreme = false;
-		int maxAttempts = 50; //For farming Extreme
+		boolean extreme = true;
+		int maxAttempts;
+		int extremeAttempts = 10; //For farming Extreme
 		
 		Login login = new Login();
 		WebDriver driver = login.login();
 		PlayAgain playAgain = new PlayAgain();
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 		WebDriverWait longWait = new WebDriverWait(driver, Duration.ofSeconds(120));
-		//driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[2]/div[2]/div[3]/div/img")).click();  //Imagination Overdrive banner
-		driver.get("https://game.granbluefantasy.jp/#quest/extra/event/6043"); 
-		System.out.println("ZodiaCamp 2nd");  
+		driver.get("https://game.granbluefantasy.jp/#quest/extra/event/6043");
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.className("atx-lead-link")));
 		System.out.println("Event SP Quests");		
+		System.out.println(driver.findElement(By.className("atx-lead-link")).getText());
 		int attempts;
 
 		//Maniac Loop
@@ -34,26 +35,41 @@ public class EventOld {
 				maxAttempts = 2; //Maniac 2x daily limit
 				attempts = 0;
 				if (attempts < maxAttempts) {
-					By cssManiac = By.cssSelector("div[data-difficulty='" + i + "']");				
+					By cssManiac = By.cssSelector("div[data-difficulty='" + i + "']");
 					wait.until(ExpectedConditions.urlContains("#quest/extra/event"));
 					wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[data-group='1']")));
 					driver.findElement(By.cssSelector("div[data-group='1']")).click(); //Solo
 					wait.until(ExpectedConditions.elementToBeClickable(By.className("btn-usual-close")));
 					List<WebElement> elementExists = driver.findElements(cssManiac);
-					if (elementExists.size() > 0) {
-						System.out.println("EventOld Maniac " + i + " launching.");
-						driver.findElement(cssManiac).click(); //Maniac+
-						wait.until(ExpectedConditions.elementToBeClickable(By.className("btn-usual-ok"))); //Daily limit OK
-						maxAttempts = Integer.valueOf(driver.findElement(By.className("txt-popup-body")).getText().replaceAll("[^0-9]", ""));
-						driver.findElement(By.className("btn-usual-ok")).click(); //Maniac
-					} else {
-						System.out.println("EventOld Maniac " + i + " not found.");
-						driver.findElement(By.className("btn-usual-close")).click();
-						break;
+					for (int j=0; j<elementExists.size(); j++) {						
+						wait.until(ExpectedConditions.urlContains("#quest/extra/event"));
+						if (driver.findElements(By.className("pop-usual")).isEmpty()) {
+							wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[data-group='1']")));
+							driver.findElement(By.cssSelector("div[data-group='1']")).click(); //Solo
+							wait.until(ExpectedConditions.elementToBeClickable(By.className("btn-usual-close")));
+						}
+						elementExists = driver.findElements(cssManiac);
+						if (elementExists.size() > 0 && !elementExists.get(j).getAttribute("class").contains("disable")) {
+							System.out.println("EventOld Maniac " + i + "(" + j + ") launching.");
+							elementExists.get(j).click(); //Maniac+
+							wait.until(ExpectedConditions.elementToBeClickable(By.className("btn-usual-ok"))); //Daily limit OK
+							maxAttempts = Integer.valueOf(driver.findElement(By.className("txt-popup-body")).getText().replaceAll("[^0-9]", ""));
+							System.out.println("With " + maxAttempts + " attempts.");
+							driver.findElement(By.className("btn-usual-ok")).click(); //Maniac
+							playAgain.playAgain(driver, longWait, maxAttempts, false);
+						} else {
+							System.out.println("EventOld Maniac " + i + "(" + j + ") not found.");
+							if (j + 1 == elementExists.size()) {
+								wait.until(ExpectedConditions.elementToBeClickable(By.className("btn-usual-close")));
+								driver.findElement(By.className("btn-usual-close")).click();
+							}
+							continue;
+						}
 					}
-
-				}
-				playAgain.playAgain(driver, longWait, maxAttempts);
+				}				
+			}
+			if (!driver.findElements(By.className("pop-usual")).isEmpty()) {
+				driver.findElement(By.className("btn-usual-close")).click();
 			}
 		}
 		
@@ -66,22 +82,25 @@ public class EventOld {
 				wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[data-group='3']")));
 				driver.findElement(By.cssSelector("div[data-group='3']")).click(); //Raid
 				wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[data-difficulty='4']")));
-				driver.findElement(By.cssSelector("div[data-difficulty='4']")).click(); //Extreme+
+				driver.findElement(By.cssSelector("div[data-difficulty='9']")).click(); //Extreme+
+				//driver.findElement(By.cssSelector("div[data-difficulty='4']")).click(); //Extreme
 			}
-			playAgain.playAgain(driver, longWait, maxAttempts);							
+			playAgain.playAgain(driver, longWait, maxAttempts, false);							
 		}
 		
 		//Extreme Farming
 		if ( extreme ) {
+			maxAttempts = extremeAttempts;
 			attempts = 0;		
 			if (attempts < maxAttempts) {
 				wait.until(ExpectedConditions.urlContains("#quest/extra/event"));
 				wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[data-group='1']")));
 				driver.findElement(By.cssSelector("div[data-group='1']")).click(); //Solo
 				wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[data-difficulty='4']")));
-				driver.findElement(By.cssSelector("div[data-difficulty='4']")).click(); //Extreme
+				driver.findElement(By.cssSelector("div[data-difficulty='9']")).click(); //Extreme+
+				//driver.findElement(By.cssSelector("div[data-difficulty='4']")).click(); //Extreme
 			}		
-			playAgain.playAgain(driver, longWait, maxAttempts);
+			playAgain.playAgain(driver, longWait, maxAttempts, false);
 		}
 		System.out.println("Script complete");
 	}
