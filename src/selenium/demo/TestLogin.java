5e5ec7ec43602c70a6dffb012b53d4615fc430af
NicholasLTan <nicholas.l.tan@gmail.com>
2026-05-27 02:50:11 -0400

    Update to use WebDriverManager

--- src/selenium/demo/TestLogin.java
+++ src/selenium/demo/TestLogin.java
@@ -4,18 +4,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class TestLogin {
 	
 	public WebDriver testLogin() throws InterruptedException {
 
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("debuggerAddress","localhost:9222");
+		WebDriver driver = new ChromeDriver(options);
 	  // Optional. If not specified, WebDriver searches the PATH for chromedriver.
-	  System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nick\\libs\\selenium-jars\\chromedriver-win64\\chromedriver.exe");
-	  ChromeOptions options = new ChromeOptions();
+	  
 	  //println File("C:\\Users\\Nick\\AppData\\Local\\Google\\Chrome\\User Data\\Default").getAbsolutePath();
-	  options.setExperimentalOption("debuggerAddress","localhost:9222");
+	  
 
 
-	  WebDriver driver = new ChromeDriver(options);
+	  
 	  
 	  return driver;
 	}	  
