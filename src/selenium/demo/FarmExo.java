b05f3d95f5cb998716720f705c23d6fae3902683
NicholasLTan <nicholas.l.tan@gmail.com>
2026-06-06 18:56:00 -0400

    Updating item tracker, new event strings

--- src/selenium/demo/FarmExo.java
+++ src/selenium/demo/FarmExo.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 
 import org.openqa.selenium.*;
 import org.junit.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FarmExo {
@@ -16,17 +17,18 @@ public class FarmExo {
 		AutoBattle autoBattle = new AutoBattle();
 		WebDriverWait wait = new WebDriverWait(driver, Objects.requireNonNull(Duration.ofSeconds(60)));
 		WebDriverWait longWait = new WebDriverWait(driver, Objects.requireNonNull(Duration.ofSeconds(1000)));
-		driver.get("https://game.granbluefantasy.jp/#event/godslayer"); //Exo event URL
-
-		System.out.println("Exo Cocytus");
-		Thread.sleep(2000);
-		driver.findElement(By.className("img-boss-quest")).click();
-		System.out.println("Crucible");
-		Thread.sleep(1000);
 		
-		String itemID = "10584"; //Stirian Lapis for Cocytus		
+		driver.get("https://game.granbluefantasy.jp/#event/godslayer"); //Exo event URL
+		By bossQuestBy = By.className("img-boss-quest");
+		wait.until(ExpectedConditions.elementToBeClickable(bossQuestBy));
+		System.out.println(driver.findElement(By.className("prt-head-current")).getText());
+		driver.findElement(bossQuestBy).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("prt-popup-header")));
+		System.out.println(driver.findElement(By.className("prt-popup-header")).getText());
+		
+		String itemID = "10596"; //Ardens Lapis for Ifrit		
 		int maxItem = 10;
-		int currItem = maxItem;
+		int currItem = 0;
 		String itemCSS = "figure[data-item-id='" + itemID + "']";
 		IsElementPresent ePresent = new IsElementPresent();
 		boolean	elementExists = ePresent.isElementPresent(driver, By.cssSelector(itemCSS));
@@ -35,42 +37,28 @@ public class FarmExo {
 			String itemCountString = item.findElement(By.xpath("./figcaption")).getText();
 			System.out.println(itemCountString);
 			String currItemCountString = itemCountString.substring(0,itemCountString.indexOf("/"));
-			System.out.println(currItemCountString);
+			maxItem = Integer.valueOf(itemCountString.substring(itemCountString.indexOf("/")+1,itemCountString.length()));
 			currItem = Integer.valueOf(currItemCountString);
-			System.out.println("currItem = " + currItem);
+			System.out.println("currItem = " + currItem + "; maxItem = " + maxItem);
 		}
 		
-		int maxAttempts = 6; // To prevent infinite loops; 0 for infinite
+		int maxAttempts = 40; // To prevent infinite loops; 0 for infinite
 		int attempts = 0;
-		//wait.until(ExpectedConditions.urlContains("#quest/extra/event"));
 		if (attempts < maxAttempts || maxAttempts == 0) {
-			//driver.findElement(By.cssSelector("[data-group='1']")).click(); //Solo
-			//Thread.sleep(2000);
-			//driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[3]/div[5]/div/div[3]/div[2]")).click(); //Play
 			driver.findElement(By.className("btn-set-quest")).click(); //Play
-			Thread.sleep(5000);          
-			//driver.findElement(By.className("btn-usual-ok")).click(); //Maniac
-			//Thread.sleep(2000);
+			wait.until(ExpectedConditions.urlContains("https://game.granbluefantasy.jp/#quest/supporter"));
 		}		
-		while (attempts < maxAttempts || maxAttempts == 0 ) {
+		while (attempts < maxAttempts || maxAttempts == 0 || currItem >= maxItem) {
 			confirmTeam.confirmTeam(wait);
 			autoBattle.autoBattle(driver, wait);
 			ePresent = new IsElementPresent();				
 			Results results = new Results();
 			results.results(driver, longWait, true);
-
-			/*
-			 * boolean elementExists = ePresent.isElementPresent(driver,
-			 * By.className("btn-usual-close")); if (elementExists) { Thread.sleep(1000);
-			 * driver.findElement(By.className("btn-usual-close")).click();
-			 * System.out.println("Mission Close"); Thread.sleep(2000); }
-			 */
+			String itemCountString = driver.findElement(By.cssSelector(itemCSS)).findElement(By.xpath("./figcaption")).getText();
+			currItem = Integer.valueOf(itemCountString.substring(0,itemCountString.indexOf("/")));
 			attempts++;
-			System.out.println(attempts);
+			System.out.println(itemCountString);
+			System.out.println(attempts + "/" + maxAttempts + " attempts");
 		}
-		
-		
-				
-		
 	}
 }
