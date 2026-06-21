c170acb2505802f339d4dc9c71a036079a8f4454
NicholasLTan <nicholas.l.tan@gmail.com>
2026-06-21 16:22:16 -0400

    Increase sleep  to prevent ClickInterceptedException

--- src/selenium/demo/JourneyDropsActivate.java
+++ src/selenium/demo/JourneyDropsActivate.java
@@ -30,7 +30,7 @@ public class JourneyDropsActivate {
 				actions.sendKeys(Keys.PAGE_DOWN).perform();
 				//wait.until(ExpectedConditions.visibilityOfElementLocated(jdByCSS));
 				wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("atx-lead-link")));
-				Thread.sleep(500);  //Necessary sleep for PGDN to process
+				Thread.sleep(1000);  //Necessary sleep for PGDN to process
 			}
 			String jdTarget = String.valueOf(jdNum - (Integer.valueOf(hours) * 10));			 
 			WebElement button = driver.findElement(jdByCSS);
