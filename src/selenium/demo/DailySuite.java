da174ba53d321fbfa88aec0927c78bc2678c4f07
NicholasLTan <nicholas.l.tan@gmail.com>
2026-06-28 18:22:42 -0400

    Update to current events

--- src/selenium/demo/DailySuite.java
+++ src/selenium/demo/DailySuite.java
@@ -8,19 +8,13 @@ import org.junit.platform.suite.api.Suite;
 	DrawCrate.class, 
 	Provisions.class, 
 	JourneyDropsActivate.class, 
-	ProQuests.class, 
+	ProQuests.class,
+	EventNew.class,
 	FarmRaids.class,
 	PendingBattles.class,
-	FarmVeritas.class,
-	FarmUnF.class
+	FarmVeritas.class
 	})
 public class DailySuite {
-
 }
-//#wrapper > div.contents > div.cnt-mypage > div.cnt-layout-v2 > div.prt-user-scene > div.prt-vyrnsampo.display-off-target.is-vyrnsampo-type3
-//<div class="btn-vyrnsampo type-3 frame-0" data-href="vyrnsampo"> </div>
 
-
-//done? - provision handle zero
-//handle event mission in pro quest
-//event old handle nightmare spawn multi/loop 
+//handle event mission in pro quest 
