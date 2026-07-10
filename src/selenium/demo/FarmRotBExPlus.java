9d28f203ef2502007cd1e4cdcb2743674c790fea
NicholasLTan <nicholas.l.tan@gmail.com>
2026-07-10 18:12:37 -0400

    New class to farm the RotB EX+ bosses
    Todo: handle strike time boss selection

---
+++ src/selenium/demo/FarmRotBExPlus.java
@@ -0,0 +1,36 @@
+package selenium.demo;
+import java.time.Duration;
+import java.util.Objects;
+
+import org.openqa.selenium.*;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FarmRotBExPlus {
+	@Test
+	public void farmSpecialQuest() throws InterruptedException {
+		int maxAttempts = 500; // Optional: To prevent infinite loops
+		By selectOne = By.cssSelector("div[class^='btn-event-raid multi'][data-chapter-id^='711']"); //Ex+
+		//By selectTwo = By.cssSelector("div[data-chapter-id='74346']"); //150
+		//By selectOne = By.cssSelector("div[class='prt-raid-image']"); //Extreme+ Elemental
+		final String eventUrl = "https://game.granbluefantasy.jp/#event/advent";
+		
+		Login login = new Login();
+		WebDriver driver = Objects.requireNonNull(login.login());
+		WebDriverWait wait = new WebDriverWait(driver, Objects.requireNonNull(Duration.ofSeconds(600)));
+		PlayAgain playAgain = new PlayAgain();
+		
+		driver.get(eventUrl);
+		System.out.println("Rise of the Beasts");
+		
+		wait.until(ExpectedConditions.elementToBeClickable(selectOne));
+		maxAttempts = Integer.valueOf(driver.findElement(selectOne).findElement(By.className("prt-stock-count")).getText());
+		driver.findElement(selectOne).click();
+		//wait.until(ExpectedConditions.elementToBeClickable(selectTwo));
+		//driver.findElement(selectTwo).click();
+		playAgain.playAgain(driver, wait, maxAttempts, false);
+		System.out.println("Script Complete");
+	}
+}
